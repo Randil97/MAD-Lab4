7735ea8307a9de8de3d29cf799ed2be6ccb43487
Randil97 <randilgurusinghe@mail.com>
2020-03-24 17:01:22 +0530

    lab 04

--- app/src/main/java/com/example/lab04/MainActivity.java
+++ app/src/main/java/com/example/lab04/MainActivity.java
@@ -1,30 +1,36 @@
 package com.example.lab04;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
+import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String CHANNEL_ID = "project";
-
+    private Button goBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button goBtn = (Button) findViewById(R.id.signup);
+        goBtn = (Button) findViewById(R.id.signup);
         goBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent Intent = new Intent(MainActivity.this,NewActivity.class);
-                startActivity(Intent);
+                addNotification();
+               // Intent Intent = new Intent(MainActivity.this,NewActivity.class);
+                //startActivity(Intent);
             }
         });
 
@@ -38,5 +44,24 @@ public class MainActivity extends AppCompatActivity {
             NotificationManager notificationManager = getSystemService(NotificationManager.class);
             notificationManager.createNotificationChannel(channel);
         }
+    }
+    public void addNotification(){
+
+        EditText n1 = (EditText) findViewById(R.id.editText);
+        String msg = n1.getText().toString();
+
+        Intent intent = new Intent(this, NewActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_launcher_background)
+                .setContentTitle("Sign up")
+                .setContentText("Hello "+ msg + "..! Welcome to Mad Team")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true);
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+        notificationManager.notify(0, builder.build());
     }
 }
